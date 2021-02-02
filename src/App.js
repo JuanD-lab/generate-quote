d1cf4ead025e7629d1505d7c28aa93f4d91a04db
JuanD-lab <yo-juan@outlook.com>
2021-02-02 15:16:07 -0500

    comments added

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
 import './App.css';
 import Screen from './components/Screen'
 
-/* const Screen = () => {
-  const [actualColor, setActualColor] = useState('#292a2b'),
-    changeColor = () => {
-        const randomNumber = (number) => {
-          return (Math.random()*number).toFixed(0);
-        }
-        
-        function colorRGB(){
-          let coolor = "("+randomNumber(255)+"," + randomNumber(255) + "," + randomNumber(255) +")"
-          console.log("rgb" + coolor)
-          return "rgb" + coolor;
-        }
-        setActualColor(colorRGB)
-    }
-  return(
-    <div style={{backgroundColor: actualColor, width: "100%"}}>
-      <QuoteBox bgColor={changeColor}/>
-    </div>
-  )
-}
-
-
-const QuoteBox = ({bgColor}) => {
-  const [viewedQuote, setChangeQuote] = useState(quotes.quotes[0]),
-    randomQuote = () => {
-      const randomNumber = Math.floor(Math.random()*101)
-      setChangeQuote(quotes.quotes[randomNumber])
-
-    }
-
-  return (
-    <div>
-      <div className="blockquote-wrapper">
-        <div className="blockquote">
-          <h1>
-           {viewedQuote.quote}
-          </h1>
-          <h4>&mdash;{viewedQuote.author}</h4>
-          <button onClick={() => { randomQuote(); bgColor();}}>New quote</button>
-        </div>
-      </div>
-    </div>
-  )
-} */
-
 function App() {
   return (
     <div className="App">
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,19 +4,21 @@ import QuoteBox from './QuoteBox'
 const Screen = () => {
     const [actualColor, setActualColor] = useState('#292a2b'),
       changeColor = () => {
+          /* las dos funcione generanun numero y crean el texto "RGB" respectivamente */
           const randomNumber = (number) => {
             return (Math.random()*number).toFixed(0);
           }
-          
           function colorRGB(){
             let coolor = "("+randomNumber(255)+"," + randomNumber(255) + "," + randomNumber(255) +")"
             console.log("rgb" + coolor)
             return "rgb" + coolor;
           }
+          /* set toma como parametro colorRGB que retorna un color RGB en texto que será usado como valor de una propiedad css para el color de Screen */ 
           setActualColor(colorRGB)
       }
     return(
       <div style={{backgroundColor: actualColor, width: "100%"}}>
+        {/* paso como un prop la funcion que genera un color para que pueda ser ejecutado por el boton dentro del coponente QuoteBox */}
         <QuoteBox bgColor={changeColor}/>
       </div>
     )
